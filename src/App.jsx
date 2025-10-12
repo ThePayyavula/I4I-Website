bd5f4e035b3b5acd6ba08ea2915ed86e5f8e6ca9
Rohan Payyavula <rohan.payyavula.cs@gmail.com>
2025-10-11 23:54:03 -0400

    set up

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { Home, Route } from "lucide-react";
+import { BrowserRouter, Routes } from "react-router-dom";
+
+
+function App() {
+
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route index element = {<Home />}/>
+          <Route path="*" />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App
